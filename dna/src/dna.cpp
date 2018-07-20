6f7e002a8811838185ca9c9092a0dcfac6e211b4
Mattias Evensson <mattias@evensson.eu>
2018-07-20 14:45:32 +0200

    Fix dna code and add sample input

--- dna/src/dna.cpp
+++ dna/src/dna.cpp
@@ -1,6 +1,29 @@
 #include "dna.hpp"
 
-std::tuple<int, int, int, int> dna(const std::string& input)
+std::tuple<int, int, int, int> dna(const std::string& symbols)
 {
-    return std::make_tuple(0, 0, 0, 0);
+    auto numA = 0;
+    auto numC = 0;
+    auto numG = 0;
+    auto numT = 0;
+    for (auto&& symbol : symbols)
+    {
+        switch (symbol)
+        {
+        case 'A':
+            numA += 1;
+            break;
+        case 'C':
+            numC += 1;
+            break;
+        case 'G':
+            numG += 1;
+            break;
+        case 'T':
+            numT += 1;
+            break;
+        }
+    }
+
+    return std::make_tuple(numA, numC, numG, numT);
 }
--- dna/test/dna_test.cpp
+++ dna/test/dna_test.cpp
@@ -15,9 +15,95 @@ SCENARIO("dna strings", "[dna]")
             THEN("the return 0 A, 0 C, 0 G, and 0 T")
             {
                 CHECK(a == 0);
-                CHECK(g == 0);
                 CHECK(c == 0);
+                CHECK(g == 0);
                 CHECK(t == 0);
+            }
+        }
+    }
+    GIVEN("an input string with two A's")
+    {
+        const std::string input = {"AA"};
+
+        WHEN("counting DNA nucleotides")
+        {
+            auto [a, c, g, t] = dna(input);
+
+            THEN("the return 2 A, 0 C, 0 G, and 0 T")
+            {
+                CHECK(a == 2);
+                CHECK(c == 0);
+                CHECK(g == 0);
+                CHECK(t == 0);
+            }
+        }
+    }
+    GIVEN("an input string with two C's")
+    {
+        const std::string input = {"CC"};
+
+        WHEN("counting DNA nucleotides")
+        {
+            auto [a, c, g, t] = dna(input);
+
+            THEN("the return 0 A, 2 C, 0 G, and 0 T")
+            {
+                CHECK(a == 0);
+                CHECK(c == 2);
+                CHECK(g == 0);
+                CHECK(t == 0);
+            }
+        }
+    }
+    GIVEN("an input string with two G's")
+    {
+        const std::string input = {"GG"};
+
+        WHEN("counting DNA nucleotides")
+        {
+            auto [a, c, g, t] = dna(input);
+
+            THEN("the return 0 A, 0 C, 2 G, and 0 T")
+            {
+                CHECK(a == 0);
+                CHECK(c == 0);
+                CHECK(g == 2);
+                CHECK(t == 0);
+            }
+        }
+    }
+    GIVEN("an input string with two T's")
+    {
+        const std::string input = {"TT"};
+
+        WHEN("counting DNA nucleotides")
+        {
+            auto [a, c, g, t] = dna(input);
+
+            THEN("the return 0 A, 0 C, 0 G, and 2 T")
+            {
+                CHECK(a == 0);
+                CHECK(c == 0);
+                CHECK(g == 0);
+                CHECK(t == 2);
+            }
+        }
+    }
+    GIVEN("an input string with lots of A, C, G, and T's")
+    {
+        const std::string input = {"AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGG"
+                                   "ATTAAAAAAAGAGTGTCTGATAGCAGC"};
+
+        WHEN("counting DNA nucleotides")
+        {
+            auto [a, c, g, t] = dna(input);
+
+            THEN("the return the count of A, C, G, and T's")
+            {
+                CHECK(a == 20);
+                CHECK(c == 12);
+                CHECK(g == 17);
+                CHECK(t == 21);
             }
         }
     }
