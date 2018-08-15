15b22a78928e6dc9bd5b5f214cb8ccc95fc8531b
Mattias Evensson <mattias@evensson.eu>
2018-08-15 13:26:58 +0200

    Add command line parsing

--- dna/src/main.cpp
+++ dna/src/main.cpp
@@ -1,6 +1,37 @@
 #include "dna.hpp"
 
+#include <boost/program_options.hpp>
+#include <cstdlib>
 #include <iostream>
+
+using namespace boost::program_options;
+
+static auto parse_options(const int argc, const char* const argv[])
+{
+    auto options = options_description{"Options"};
+    options.add_options()("help,h", "Display this help message.");
+
+    auto parse_error = false;
+    auto vm = variables_map{};
+    try
+    {
+        store(parse_command_line(argc, argv, options), vm);
+        notify(vm);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << "\n\n";
+        parse_error = true;
+    }
+
+    if (argc <= 1 || parse_error || vm.count("help"))
+    {
+        auto program_name = argc >= 1 ? argv[0] : "dna";
+        std::cout << "Usage: " << program_name << " [options]\n\n";
+        std::cout << options;
+        std::exit(EXIT_SUCCESS);
+    }
+}
 
 static auto readAllInput()
 {
@@ -10,8 +41,10 @@ static auto readAllInput()
     return input;
 }
 
-int main()
+int main(const int argc, const char* const argv[])
 {
+    parse_options(argc, argv);
+
     auto input = readAllInput();
     auto [a, c, g, t] = dna(input);
     std::cout << a << " " << c << " " << g << " " << t << "\n";
