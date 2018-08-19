440d17e427024b9b7df5ee018fe515870cd45b76
Mattias Evensson <mattias@evensson.eu>
2018-08-19 16:39:38 +0200

    Implement RNA transcription

--- src/rna/src/rna.cpp
+++ src/rna/src/rna.cpp
@@ -1,8 +1,12 @@
 #include "rna.hpp"
 
+#include <algorithm>
 #include <string>
 
-auto rna(const std::string_view & /*symbols*/) -> std::string
+auto rna(const std::string_view& symbols) -> std::string
 {
-    return "";
+    auto result = std::string{};
+    result.resize(symbols.size());
+    std::replace_copy(symbols.begin(), symbols.end(), result.begin(), 'T', 'U');
+    return result;
 }
--- src/rna/test/rna_test.cpp
+++ src/rna/test/rna_test.cpp
@@ -14,11 +14,84 @@ SCENARIO("rna strings", "[rna]")
 
         WHEN("transcribing DNA into RNA")
         {
-            auto result = rna(input); // NOLINT
+            auto result = rna(input);
 
             THEN("the return empty string")
             {
                 CHECK(result == ""s);
+            }
+        }
+    }
+    GIVEN("an input string with two A's")
+    {
+        const auto input = "AA"s;
+
+        WHEN("transcribing DNA into RNA")
+        {
+            auto result = rna(input);
+
+            THEN("the return sring with two A's")
+            {
+                CHECK(result == "AA"s);
+            }
+        }
+    }
+    GIVEN("an input string with two C's")
+    {
+        const auto input = "CC"s;
+
+        WHEN("transcribing DNA into RNA")
+        {
+            auto result = rna(input);
+
+            THEN("the return string with two C's")
+            {
+                CHECK(result == "CC"s);
+            }
+        }
+    }
+    GIVEN("an input string with two G's")
+    {
+        const auto input = "GG"s;
+
+        WHEN("transcribing DNA into RNA")
+        {
+            auto result = rna(input);
+
+            THEN("the return string with two G's")
+            {
+                CHECK(result == "GG"s);
+            }
+        }
+    }
+    GIVEN("an input string with two T's")
+    {
+        const auto input = "TT"s;
+
+        WHEN("transcribing DNA into RNA")
+        {
+            auto result = rna(input);
+
+            THEN("the return string with two U's")
+            {
+                CHECK(result == "UU"s);
+            }
+        }
+    }
+    GIVEN("an input string with lots of A, C, G, and T's")
+    {
+        const auto input =
+            "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"s;
+
+        WHEN("transcribing DNA into RNA")
+        {
+            auto result = rna(input);
+
+            THEN("the return string with all T's replaced with U's")
+            {
+                CHECK(
+                    result
+                    == "AGCUUUUCAUUCUGACUGCAACGGGCAAUAUGUCUCUGUGUGGAUUAAAAAAAGAGUGUCUGAUAGCAGC"s);
             }
         }
     }
