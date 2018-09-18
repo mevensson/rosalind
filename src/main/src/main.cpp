20e03294168e745ad2fd35e67f87397e7a5e25f8
Mattias Evensson <mattias@evensson.eu>
2018-09-18 22:31:39 +0200

    Remove some duplication in main

--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -6,8 +6,26 @@
 #include <boost/iostreams/device/mapped_file.hpp>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 
 using namespace boost::iostreams;
+
+template<class SerFunc, class ParFunc>
+static auto execute(
+    const ExecutionType executionType,
+    const SerFunc serial,
+    const ParFunc parallel)
+{
+    switch (executionType.type())
+    {
+    case ExecutionType::Type::Serial:
+        return serial();
+    case ExecutionType::Type::Parallel:
+        return parallel();
+    default:
+        throw std::domain_error("Unknown excution type");
+    }
+}
 
 int main(const int argc, const char* const argv[])
 {
@@ -17,30 +35,33 @@ int main(const int argc, const char* const argv[])
     const auto mappedFile = mapped_file_source{options.filename()};
     const auto first = mappedFile.begin();
     const auto last = mappedFile.end();
+    const auto numThreads = executionType.numThreads();
     switch (options.problem())
     {
     case Problem::Dna:
     {
-        auto [a, c, g, t] = // NOLINT
-            executionType.type() == ExecutionType::Type::Parallel
-            ? dna_par(first, last, executionType.numThreads())
-            : dna_ser(first, last);
+        auto [a, c, g, t] = execute( // NOLINT
+            executionType,
+            [&]() { return dna_ser(first, last); },
+            [&]() { return dna_par(first, last, numThreads); });
         std::cout << a << " " << c << " " << g << " " << t << "\n";
         break;
     }
     case Problem::Rna:
     {
-        auto result = executionType.type() == ExecutionType::Type::Parallel
-            ? rna_par(first, last, executionType.numThreads())
-            : rna_ser(first, last);
+        auto result = execute(
+            executionType,
+            [&]() { return rna_ser(first, last); },
+            [&]() { return rna_par(first, last, numThreads); });
         std::cout << result << "\n";
         break;
     }
     case Problem::Revc:
     {
-        auto result = executionType.type() == ExecutionType::Type::Parallel
-            ? revc_par(first, last, executionType.numThreads())
-            : revc_ser(first, last);
+        auto result = execute(
+            executionType,
+            [&]() { return revc_ser(first, last); },
+            [&]() { return revc_par(first, last, numThreads); });
         std::cout << result << "\n";
         break;
     }
